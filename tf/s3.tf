a268034459672992a6bf0bd83c3cc854590fe0cb
Rafael Jose D <rafaeljdm21@hotmail.com>
2025-02-25 02:51:49 -0300

    Update S3 bucket references and adjust zip file paths in user data scripts

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,16 +1,6 @@
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
 resource "aws_s3_bucket" "code_bucket" {
-  bucket = "lti-project-code-bucket-${random_string.bucket_suffix.result}"
-}
-
-resource "aws_s3_bucket_acl" "code_bucket_acl" {
-  bucket = aws_s3_bucket.code_bucket.bucket
-  acl    = "private"
+  bucket        = "lti-project-code-bucket-rjd"
+  force_destroy = true
 }
 
 resource "null_resource" "generate_zip" {
@@ -27,12 +17,12 @@ resource "aws_s3_object" "backend_zip" {
   bucket     = aws_s3_bucket.code_bucket.bucket
   key        = "backend.zip"
   source     = "../backend.zip"
-  depends_on = [null_resource.generate_zip]
+  depends_on = [aws_s3_bucket.code_bucket, null_resource.generate_zip]
 }
 
 resource "aws_s3_object" "frontend_zip" {
   bucket     = aws_s3_bucket.code_bucket.bucket
   key        = "frontend.zip"
   source     = "../frontend.zip"
-  depends_on = [null_resource.generate_zip]
+  depends_on = [aws_s3_bucket.code_bucket, null_resource.generate_zip]
 }
